e267c922e32333645353f98704a6b199b8c353bb
Tio <atiohaidar@gmail.com>
2023-04-17 08:01:23 +0700

    menambahkan API Movies

---
+++ modul9_1302210057/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace modul9_1302210057
+{
+    public class Movie
+    {
+        public String title { get; set; }
+        public String director { get; set; }
+        public List<String> start { get; set; }
+        public String description { get; set; }
+
+        public Movie(String title, String director, List<String> start, String description) { 
+            this.title = title;
+            this.director = director;
+            this.start = start;
+            this.description = description;
+
+        }
+
+        
+    }
+}
--- modul9_1302210057/Program.cs
+++ modul9_1302210057/Program.cs
@@ -1,3 +1,41 @@
+using System.IO;
+using modul9_1302210057;
+List<String> stars1 = new List<String>();
+stars1.Add("Tim Robbins");
+stars1.Add("Morgan Freeman");
+stars1.Add("Bob Gunton");
+stars1.Add("William Sadler");
+List<String> stars2 = new List<String>();
+stars2.Add("Marlon Brando");
+stars2.Add("Al Pacino");
+stars2.Add("James Caan");
+stars2.Add("Diane Keaton");
+
+List<String> stars3 = new List<String>();
+stars3.Add("Christian Bale");
+stars3.Add("Heath Ledger");
+stars3.Add("Aaron Eckhart");
+stars3.Add("Michael Caine");
+List<Movie> movies = new List<Movie>();
+movies.Add(new Movie(
+    "The Shawshank Redemption",
+    "Frank Darabont",
+    stars1,
+    "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion."
+)); 
+movies.Add(new Movie(
+    "The Godfather",
+    "Francis Ford Coppola",
+    stars2 ,
+    "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."
+)); movies.Add(new Movie(
+    "The Dark Knight",
+    "Christopher Nolan",
+    stars3 ,
+    "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+)); 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +58,25 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapGet("/api/Movies", () =>
+{
+    return movies;
+});
+app.MapGet("/api/Movies/{index}", (int index) =>
+{
+    return movies[index];
+}
+    );
+app.MapPost("api/Movies", (Movie movie) =>
+
+{
+    movies.Add(movie);
+    return "berhasil";
+});
+
+app.MapDelete("api/Movies/{index}", (int index)=>{
+    movies.RemoveAt(index);
+    return "berhasil dihapus";
+});
 
 app.Run();
